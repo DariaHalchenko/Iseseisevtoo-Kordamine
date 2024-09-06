a30e51092360bae8e73a003cbde90b08df27e1df
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-09-06 21:31:52 +0300

    Update Ulesanne _2.cs

--- Iseseisevtoo-Kordamine/Ulesanne _2.cs
+++ Iseseisevtoo-Kordamine/Ulesanne _2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,28 @@ namespace Iseseisevtoo_Kordamine
 {
     internal class Ulesanne__2
     {
+        public static int[] Arvude_massiv(int N)
+        {
+            Random random = new Random();
+            Console.WriteLine(N);
+            int[] arvud = new int[N];
+            for (int i = 0; i < arvud.Length; i++)
+            {
+                arvud[i] = random.Next(-150, 151);
+           } 
+            return arvud;
+        }
+        public static int Elementide_arv (int[] arvud, int min, int max)
+        {
+            int elemendid  = 0;
+            foreach (int arv in arvud)
+            {
+                if (arv >= min && arv <= max)
+                {
+                    elemendid +=1 ;
+                }
+            }
+            return elemendid ;
+        }
     }
 }
