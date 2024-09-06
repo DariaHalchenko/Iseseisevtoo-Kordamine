e438fa79cb7c60637d102dff0a2f5aa4ec388c2c
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-09-06 21:32:42 +0300

    Update Ulesanne_3.cs

--- Iseseisevtoo-Kordamine/Ulesanne_3.cs
+++ Iseseisevtoo-Kordamine/Ulesanne_3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,12 @@ namespace Iseseisevtoo_Kordamine
 {
     internal class Ulesanne_3
     {
+        public const double Maksumäär = 0.22; 
+
+        public double Tulumaksu_arvutamine(double palk)
+        {
+            double maks = palk * Maksumäär;
+            return maks;
+        }
     }
 }
