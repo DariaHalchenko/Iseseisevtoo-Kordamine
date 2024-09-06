6b50e0de80dde12e54e1d98fabe4128f3079f725
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-09-06 21:31:25 +0300

    Update MainClass.cs

--- Iseseisevtoo-Kordamine/MainClass.cs
+++ Iseseisevtoo-Kordamine/MainClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,20 +14,42 @@ namespace Iseseisevtoo_Kordamine
         public static void Main(string[] args) 
         {
             //Ü 1. Введите с клавиатуры строку произвольной длины и подсчитайте процент вхождения гласных букв латинского алфавита в строку (не различая регистры).
-            List<char> vokaalide_loetelu = new List<char>();
             Console.OutputEncoding = Encoding.UTF8;
             Console.Write("Sisestage sõna või lause: ");
             string tekst = Console.ReadLine();
-            double protsent = Ulesanne_1.häälikute_loendamine(tekst, vokaalide_loetelu);
+            double protsent = Ulesanne_1.häälikute_loendamine(tekst);
             Console.WriteLine("Vokaalide esinemise protsent: " + protsent + "%");
-            if (vokaalide_loetelu.Count > 0)
+            
+            //Ü 2. Задан массив действительных чисел из N элементов (используйте генератор случайных чисел). Определить количество элементов, значения которых находятся в диапазоне от -100 до +100.
+            Console.Write("Sisesta elementide arv: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] arvud = Ulesanne__2.Arvude_massiv(N);
+            Console.WriteLine("Toimivate numbrite massiiv N-elementidest: ");
+            foreach (int arv in arvud)
             {
-                Console.WriteLine("Vokaali: " + string.Join (", " ,vokaalide_loetelu));
-            }
-            else
+                Console.Write(arv + " ");
+            } 
+            int elemendid = Ulesanne__2.Elementide_arv(arvud, -100, 100);
+            Console.WriteLine("Elemendid vahemikus -100 kuni 100: " + elemendid);
+
+            //Ü 3. Написать подпрограмму, которая вычисляет подоходный налог для введеной пользователем зарплаты.
+            try
             {
-                Console.WriteLine("Vokaalide ei leitud");
+                Console.Write("Milline on teie palk? ");
+                double palk = double.Parse(Console.ReadLine());
+                if (palk < 0)
+                {
+                    Console.WriteLine("Viga");
+                    return;
+                }
+                Ulesanne_3 ulesanne_3 = new Ulesanne_3();
+                double maks = ulesanne_3.Tulumaksu_arvutamine(palk);
+                Console.WriteLine("Teie tulumaks on:" + maks + "евро");
             }
-        }
+            catch (FormatException)
+            {
+                Console.WriteLine("Sisesta positiivne arv");
+            }
+        }    
     }
 }
