acfc39229ef027bb7271e0dbcaed462b95fe2175
opilane <opilane@HP-CZC2349HTC>
2024-09-06 14:13:42 +0300

    Add project files.

---
+++ Iseseisevtoo-Kordamine/MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//5. Variant
+
+namespace Iseseisevtoo_Kordamine
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args) 
+        {
+            //Ü 1. Введите с клавиатуры строку произвольной длины и подсчитайте процент вхождения гласных букв латинского алфавита в строку (не различая регистры).
+            List<char> vokaalide_loetelu = new List<char>();
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Sisestage sõna või lause: ");
+            string tekst = Console.ReadLine();
+            double protsent = Ulesanne_1.häälikute_loendamine(tekst, vokaalide_loetelu);
+            Console.WriteLine("Vokaalide esinemise protsent: " + protsent + "%");
+            if (vokaalide_loetelu.Count > 0)
+            {
+                Console.WriteLine("Vokaali: " + string.Join (", " ,vokaalide_loetelu));
+            }
+            else
+            {
+                Console.WriteLine("Vokaalide ei leitud");
+            }
+        }
+    }
+}
---
+++ Iseseisevtoo-Kordamine/Ulesanne_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtoo_Kordamine
+{
+    internal class Ulesanne_1
+    {
+        public static double häälikute_loendamine(string tekst, List<char> vokaalide_loetelu)
+        {
+            string vokaali = "aoeiuy";
+            int vokaali_loendama = 0;
+            vokaalide_loetelu = new List<char>();
+            foreach (char sümbol in tekst)
+            {
+                if (vokaali.Contains(sümbol))
+                {
+                    vokaali_loendama += 1;
+                    vokaalide_loetelu.Add(sümbol);
+                }
+            }
+            if (tekst.Length == 0)
+            {
+                return 0;
+
+            }
+            double protsent = (double)vokaali_loendama/tekst.Length * 100;
+            return protsent; 
+        }
+    }
+}
